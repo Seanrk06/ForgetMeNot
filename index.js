0af9bb922d23d29c65c4eeecba757a1be16cd41f
Sean K <Trueflash42@outlook.com>
2022-08-05 10:24:55 -0400

    Updates

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get("/teacher", function(req, res) {
 
 
 
-app.listen(3500, function() {
+app.listen(process.env.PORT || 3500, function() {
     console.log("server is running on port 3500");
 });
 
